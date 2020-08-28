8f92fcc27df9ffb1fa0f5cf507a0f3f6254f2a86
Mark McKelvy <mmckelvy@gmail.com>
2020-08-28 09:14:46 -0700

    cleanup

--- get-deep-prop.js
+++ get-deep-prop.js
@@ -1,14 +1,14 @@
 /**
-* Sets a deeply nested object or array property.
+* Gets a deeply nested object or array property.
 *
 * @param {object} obj -- The deeply nested object
 * @param {array} path -- The path to the object as an array. Ex: ['topkey', 'nextkey', 0, 'nestedKey']
 * @param {any} value -- The new value to set.
 
-* @return {object} -- The updated object. Note that this method mutates the original object.
+* @return {any} -- The value at the specified path.
 */
 
-module.exports = function setDeepProp(obj, path, value) {
+module.exports = function getDeepProp(obj, path, value) {
   let current = obj
 
   // Get to the proper key
